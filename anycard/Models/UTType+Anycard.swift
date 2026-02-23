6dc7e0a99db2f93278db636584effcb790599316
Michał Jaskólski <emdzej@MajBook.lan>
2026-02-23 22:51:29 +0100

    chore: rename bundle to pl.emdzej.anycard, display name to Anycard

--- anycard/Models/UTType+Anycard.swift
+++ anycard/Models/UTType+Anycard.swift
@@ -3,6 +3,6 @@ import UniformTypeIdentifiers
 extension UTType {
     /// Custom UTType for anycard export files
     static var anycard: UTType {
-        UTType(exportedAs: "com.emdzej.anycard.backup", conformingTo: .json)
+        UTType(exportedAs: "pl.emdzej.anycard.backup", conformingTo: .json)
     }
 }
